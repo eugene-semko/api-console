535275c08d1a6a65086c9435b7854a75547773f5
eugene-semko <eugene.movie.production@gmail.com>
2022-02-07 17:12:33 +0300

    Add module styling

---
+++ src/components/Authorization/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC, useEffect, useState } from "react";
+import styles from "./index.module.css";
+type propsType = {};
+export const Authorization: FC<propsType> = (props) => {
+   return <div></div>;
+};
