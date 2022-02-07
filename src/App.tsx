6241b5d7dcfe7b4df74df8755dc832193e6f64bb
eugene-semko <eugene.movie.production@gmail.com>
2022-02-07 18:11:38 +0300

    Add Authorization component Create ui component input Install HookForm and other form dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./style/index.css";
+import { Authorization } from "./components/Authorization";
+import "./App.css";
 
 function App() {
-   return <div className='App'></div>;
+   return (
+      <div className='App'>
+         <Authorization />
+      </div>
+   );
 }
 
 export default App;
--- src/components/Authorization/index.tsx
+++ src/components/Authorization/index.tsx
@@ -1,6 +1,38 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC } from "react";
 import styles from "./index.module.css";
+import { ReactComponent as LogoIcon } from "../../assets/LOGO.svg";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/Input";
+
 type propsType = {};
 export const Authorization: FC<propsType> = (props) => {
-   return <div></div>;
+   const { control, handleSubmit } = useForm();
+
+   return (
+      <div className={styles.container}>
+         <LogoIcon className={styles.logo} />
+         <form className={styles.form_container}>
+            <Input
+               control={control}
+               className={styles.input}
+               name={"login"}
+               label={"Логин"}
+            />
+            <Input
+               control={control}
+               name={"sublogin"}
+               label={"Сублогин"}
+               optionalLabel={"Опционально"}
+               className={styles.input}
+            />
+            <Input
+               control={control}
+               name={"password"}
+               type={"password"}
+               label={"Пароль"}
+               className={styles.input}
+            />
+         </form>
+      </div>
+   );
 };
